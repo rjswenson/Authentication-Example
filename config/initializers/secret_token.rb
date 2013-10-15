4d0bb73fed8e561bb06cf1029b5247e042686cc5
Robin Swenson-Healey <rjswenson@gmail.com>
2013-10-15 13:13:55 -0700

    Railscast #250

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuthenticationExample::Application.config.secret_key_base = '5424a5086a422a19db9119a06c70f8bf191d1eef9af2f6b0173422c32923baee94c9719822e0d812497b92f30080680a7d7fc1dbd55830f5534a2ee5d3b14097'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AuthenticationExample::Application.config.session_store :cookie_store, key: '_AuthenticationExample_session'
